317c145f1f3a6a2875c0fba2fa2de24a17c387d3
yang <joey674901205@gmail.com>
2022-10-03 15:55:06 -0400

    modified typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveDiary(_id: String, content: String, date: String): User
+    saveDiary(content: String!, date: String!): User
     removeDiary(_id: ID!): User
   }
 `;
